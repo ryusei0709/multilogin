8958a836f564796cba2531d3f8c87cdced9fb7a9
RyuseiTsuchiya <ryusei.baseball.0709@gmail.com>
2023-04-06 17:30:27 +0900

    属性で設定

--- resources/views/components/tests/app.blade.php
+++ resources/views/components/tests/app.blade.php
@@ -24,11 +24,6 @@
             {{ $header }}
         </header>
 
-        <div>
-            {{ $ryusei }}
-        </div>
-
-
         <div class="font-sans text-gray-900 antialiased">
             {{ $slot }}
         </div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,8 +2,9 @@
 <x-slot name="header">
 header1
 </x-slot>
+コンポネント１
+
+<x-tests.card title="タイトル" content="コンテント"></x-tests.card>
 
 
-
-  test1
 </x-tests.app>
